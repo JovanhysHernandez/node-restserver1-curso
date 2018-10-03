b8125503efe77bdddbaaeb30ef9527fdaaacbcd4
Jovanhys <jovanhys.hernandez@towasoftware.com>
2018-10-03 11:55:47 -0500

    RestServer - usuario

---
+++ server/models/usuario.js
@@ -0,0 +1,59 @@
+
+const mongoose = require('mongoose');
+const uniqueValidator = require ('mongoose-unique-validator');
+
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol válido'
+} 
+
+
+let Schema = mongoose.Schema;
+
+
+let usuarioSchema  = new Schema ({
+    nombre:{
+        type: String,
+        required: [true, 'El nombre es necesairo.']
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'El correo es obligatorio.']
+    },
+    password: {
+        type: String,
+        required: [true, 'La contraseña es obligatoria.']
+    },   
+    img:{   //no es obligatoria
+        type: String,
+        required: false
+    },
+    role: { //default: 'USER_ROLE'
+        type: String,
+        default: 'USER_ROLE',
+        enum: rolesValidos
+    },
+    estado: { //boolean
+        type: Boolean,
+        default: true
+    },
+    google: { //boolean
+        type: Boolean,
+        default: false
+    }
+});
+
+usuarioSchema.methods.toJSON = function() {
+
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+
+    return userObject;
+}
+
+
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe ser único.' });
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
