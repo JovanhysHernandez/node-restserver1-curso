1bd76ed3b833e180002e55d92f150da1b978a74d
Jovanhys <jovanhys.hernandez@towasoftware.com>
2018-10-04 15:14:51 -0500

    Login normal seccion 10

--- server/server.js
+++ server/server.js
@@ -6,15 +6,15 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
     // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
 
-
-app.use(require('./routes/usuario'));
+//Congifuración golbal de rutas.
+app.use(require('./routes/index'));
 
 mongoose.connect(process.env.URLDB, (err, res) => {
     if (err) throw err;
@@ -24,5 +24,5 @@ mongoose.connect(process.env.URLDB, (err, res) => {
 
 
 app.listen(process.env.PORT, () => {
-    console.log('Escuchando un Puerto: ', 3000)
-})  
+    console.log('Escuchando Puerto: ', process.env.PORT);
+});
